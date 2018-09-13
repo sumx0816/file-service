8400f008e1220375bb9c71543c2f785e91fc4c09
sumx <s_m_x_1234@qq.com>
2018-09-13 22:13:25 +0800

    远程配置

--- config-server/src/main/java/cn/com/hsit/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/cn/com/hsit/configserver/ConfigServerApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
